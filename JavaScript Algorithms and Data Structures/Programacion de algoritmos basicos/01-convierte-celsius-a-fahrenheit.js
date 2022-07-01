b6076c1d8e4e7e8f90456e5b0ca0db634f3df677
Mario <mariovelascocanadas@outlook.es>
2022-07-01 19:36:21 +0200

    Funcion que convierte una temperatura en Celsius a Fahrenheit

---
+++ JavaScript Algorithms and Data Structures/Programacion de algoritmos basicos/01-convierte-celsius-a-fahrenheit.js
@@ -0,0 +1,10 @@
+function convertToF(celsius) {
+    //Crear variable y aplicarle la formula matematica
+    let fahrenheit;
+
+    fahrenheit = celsius * 9 / 5 + 32;
+
+    return fahrenheit;
+}
+
+convertToF(30);
