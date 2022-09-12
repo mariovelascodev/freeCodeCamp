41f0e8696ab9d999f3c3a6656eb5127c14f8ebcb
Mario <mariovelascocanadas@outlook.es>
2022-09-12 17:54:32 +0200

    Función que suma dos argumentos. Si sólo se proporciona un argumento, entonces devuelve una función que espera un argumento y devuelve la suma

--- JavaScript Algorithms and Data Structures/Programacion de algoritmos intermedios/19-argumentos-opcionales.js
+++ JavaScript Algorithms and Data Structures/Programacion de algoritmos intermedios/19-argumentos-opcionales.js
@@ -1,22 +1,22 @@
 function addTogether() {
   //Guardar los argumentos en dos variables
-   const [first, second] = arguments;
+  const [first, second] = arguments;
 
   /*Comprobar si el primer argumento es un numero y 
   la longitud de arguments es igual a 1, si es así retornar 
   un segundo argumento a la función*/
-  if(typeof(first) === 'number' && arguments.length === 1) {
+  if (typeof (first) === 'number' && arguments.length === 1) {
     return (second) => addTogether(first, second);
-   //Si no comprobar si ambos argumentos son numeros y si no devolver undefined. 
-  }else if(typeof(first) !== 'number' || typeof(second) !== 'number') {
-     return undefined;
+    //Si no comprobar si ambos argumentos son numeros y si no devolver undefined. 
+  } else if (typeof (first) !== 'number' || typeof (second) !== 'number') {
+    return undefined;
     //Si ambos son numeros devolver la suma 
-   }else {
-     return first + second;
-   }
+  } else {
+    return first + second;
+  }
 }
 
-addTogether(2,3);
+addTogether(2, 3);
 addTogether("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 addTogether(5)(7);
 addTogether(2, "3");
