5525ff3179561cabac6a927f4f3140b0edfbe393
Mario <mariovelascocanadas@outlook.es>
2022-09-12 17:56:09 +0200

    Completar el constructor de objetos para que haga los metodos inticados

--- JavaScript Algorithms and Data Structures/Programacion de algoritmos intermedios/20-crea-una-persona.js
+++ JavaScript Algorithms and Data Structures/Programacion de algoritmos intermedios/20-crea-una-persona.js
@@ -1,33 +1,33 @@
-const Person = function(firstAndLast) {
+const Person = function (firstAndLast) {
   // Cambia solo el código debajo de esta línea
   //Crear una varible que contendrá el nombre completo
   let fullName = firstAndLast;
 
   //Métodos getter
-  this.getFirstName = function() {
+  this.getFirstName = function () {
     return fullName.split(' ')[0];
   };
 
-  this.getLastName = function() {
+  this.getLastName = function () {
     return fullName.split(' ')[1];
   };
 
-  this.getFullName = function() {
+  this.getFullName = function () {
     return fullName;
   };
 
   //Métodos setter
-   this.setFirstName = function(name) {
-     fullName = name + ' ' + fullName.split(' ')[1];
-   };
+  this.setFirstName = function (name) {
+    fullName = name + ' ' + fullName.split(' ')[1];
+  };
 
-   this.setLastName = function(name) {
-     fullName = fullName.split(' ')[0] + ' ' + name;
-   };
+  this.setLastName = function (name) {
+    fullName = fullName.split(' ')[0] + ' ' + name;
+  };
 
-   this.setFullName = function(name) {
-     fullName = name;
-   };
+  this.setFullName = function (name) {
+    fullName = name;
+  };
 
 };
 
