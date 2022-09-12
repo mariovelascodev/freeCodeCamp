db4164c0afdb9e6ba1b747688132a99a4f1190cf
Mario <mariovelascocanadas@outlook.es>
2022-09-12 17:51:08 +0200

    Función que comprueba si el predicado (segundo argumento) es truthy en todos los elementos de una colección (primer argumento)

--- JavaScript Algorithms and Data Structures/Programacion de algoritmos intermedios/18-todo-sea-verdad.js
+++ JavaScript Algorithms and Data Structures/Programacion de algoritmos intermedios/18-todo-sea-verdad.js
@@ -1,21 +1,21 @@
 function truthCheck(collection, pre) {
-  
+
   //Recorrer el arreglo
   //Comprobar si contiene algún valor falso, y si tiene devolver false y parar bucle
   //De lo contrario devolver true
-   for(let i = 0; i < collection.length; i++) {
-     if(!collection[i][pre]) {
-       return false;
-       break;
-     }
-   }
+  for (let i = 0; i < collection.length; i++) {
+    if (!collection[i][pre]) {
+      return false;
+      break;
+    }
+  }
 
-   return true;
+  return true;
 
 }
 
-truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot");
+truthCheck([{ name: "Quincy", role: "Founder", isBot: false }, { name: "Naomi", role: "", isBot: false }, { name: "Camperbot", role: "Bot", isBot: true }], "isBot");
 
-truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "name");
+truthCheck([{ name: "Quincy", role: "Founder", isBot: false }, { name: "Naomi", role: "", isBot: false }, { name: "Camperbot", role: "Bot", isBot: true }], "name");
 
-truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "role")
+truthCheck([{ name: "Quincy", role: "Founder", isBot: false }, { name: "Naomi", role: "", isBot: false }, { name: "Camperbot", role: "Bot", isBot: true }], "role")
---
+++ JavaScript Algorithms and Data Structures/Programacion de algoritmos intermedios/20-crea-una-persona.js
@@ -0,0 +1,35 @@
+const Person = function(firstAndLast) {
+  // Cambia solo el código debajo de esta línea
+  //Crear una varible que contendrá el nombre completo
+  let fullName = firstAndLast;
+
+  //Métodos getter
+  this.getFirstName = function() {
+    return fullName.split(' ')[0];
+  };
+
+  this.getLastName = function() {
+    return fullName.split(' ')[1];
+  };
+
+  this.getFullName = function() {
+    return fullName;
+  };
+
+  //Métodos setter
+   this.setFirstName = function(name) {
+     fullName = name + ' ' + fullName.split(' ')[1];
+   };
+
+   this.setLastName = function(name) {
+     fullName = fullName.split(' ')[0] + ' ' + name;
+   };
+
+   this.setFullName = function(name) {
+     fullName = name;
+   };
+
+};
+
+const bob = new Person('Bob Ross');
+bob.getFullName();
